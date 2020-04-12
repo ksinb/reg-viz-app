2f4d336fce7ba7f9510b5399d2a9b67b80cdecc7
ksinb <61133806+ksinb@users.noreply.github.com>
2020-04-13 00:21:21 +0900

    fix: nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,5 +47,19 @@ export default {
     /*
      ** You can extend webpack config here
      */
+    babel: {
+      presets({ isServer }) {
+        return [
+          [
+            require.resolve('@nuxt/babel-preset-app'),
+            // require.resolve('@nuxt/babel-preset-app-edge'), // For nuxt-edge users
+            {
+              buildTarget: isServer ? 'server' : 'client',
+              corejs: { version: 3 }
+            }
+          ]
+        ]
+      }
+    }
   }
 }
